e2f9138cda500228bad26b7dc5cc007a35977b68
ENIGMA <niteshchaudhary0812@gmail.com>
2025-02-28 20:35:40 +0530

    class-4

---
+++ src/main/java/Single_copy_C3/Single_Copy.java
@@ -0,0 +1,33 @@
+package Single_copy_C3;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class Single_Copy
+ */
+@WebServlet("/Single_Copy")
+public class Single_Copy extends HttpServlet {
+//	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		ServletContext sc = getServletContext();
+		HttpSession hs = request.getSession();
+		hs.setAttribute("sp",request.getParameter("t1"));
+		RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+		rd.forward(request, response);
+	}
+
+}
